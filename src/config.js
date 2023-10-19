ba86a0d308f963d451f3601c3dc42678fcb648ec
Lili <lile7886@gmail.com>
2023-10-18 20:16:55 -0400

    changed over new db

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL
-      || 'postgres://qnxpouhxbvnttw:527024e2bcea308a057ca2cb9d15c30ad8fe768549b9ba917450b12684b08334@ec2-18-205-44-21.compute-1.amazonaws.com:5432/d24i966cgkjm18',
+      || 'postgres://lfuen:&>K--n?OlR(Bfs9%4N6pPlTLeiqe@cqlz5glh6vqe.us-east-1.rds.amazonaws.com:5432/travelers',
     JWT_SECRET: process.env.JWT_SECRET || 'canttouchthis',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
     API_TOKEN: process.env.API_TOKEN,
--- test/setup.js
+++ test/setup.js
@@ -7,7 +7,7 @@ process.env.JWT_EXPIRY = '3h'
 require('dotenv').config()
 
 process.env.DATABASE_URL = process.env.DATABASE_URL
-  || "postgres://qnxpouhxbvnttw:527024e2bcea308a057ca2cb9d15c30ad8fe768549b9ba917450b12684b08334@ec2-18-205-44-21.compute-1.amazonaws.com:5432/d24i966cgkjm18"
+  || 'postgres://lfuen:&>K--n?OlR(Bfs9%4N6pPlTLeiqe@cqlz5glh6vqe.us-east-1.rds.amazonaws.com:5432/travelers'
 
 const { expect } = require('chai')
 const supertest = require('supertest')
